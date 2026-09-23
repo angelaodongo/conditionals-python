df87f869a65616d940798ce3c8c2713ccbd2feee
Angela Odongo <celesteodongo@gmail.com>
2026-09-23 16:18:39 +0300

    created the salutation and main functions

--- bank.py
+++ bank.py
@@ -3,3 +3,18 @@
     If the greeting starts with “hello”, output $0. If the greeting starts with an “h” (but not “hello”), output $20. 
     Otherwise, output $100. Ignore any leading whitespace in the user's greeting, and treat the user's greeting case-insensitively. 
 """
+
+def salutation(g):
+    if g == "hello":
+        return "$0"
+    elif g.startswith("h") and g != "hello":
+        return "$20"
+    else:
+        return "$100"
+
+
+def main():
+    greeting = input("Greeting: ").lstrip().lower()
+    print(salutation(greeting))
+
+main()
