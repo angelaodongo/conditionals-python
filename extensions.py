3fe681880fab3d045c977d80fd24ec5f9200a5b0
Angela Odongo <celesteodongo@gmail.com>
2026-09-23 16:56:37 +0300

    created the prog_name and main functions

--- extensions.py
+++ extensions.py
@@ -9,3 +9,31 @@
     .zip
     If the file's name ends with some other suffix or has no suffix at all, output application/octet-stream instead, which is a common default.
 """
+
+def prog_name(p):
+    match p:
+        case p.endswith(".gif"):
+            return "Image/GIF"
+        case p.endswith(".jpg"):
+            return "Image/GIF"
+        case p.endswith(".jpeg"):
+            return "Image/GIF"
+        case p.endswith(".png"):
+            return "Image/GIF"
+        case p.endswith(".pdf"):
+            return "Document"
+        case p.endswith(".txt"):
+            return "Text File"
+        case p.endswith(".zip"):
+            return "ZIP Folder"
+        case p.endswith(".gif"):
+            return "Image/GIF"
+        case _:
+            return "application/octet-stream"
+            
+                
+def main():
+    program = input("What's the name of your file? ")
+    print(prog_name(program))
+
+main()
