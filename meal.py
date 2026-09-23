847534faeb86ceac2e0e00cb05afc5b3f5ce035f
Angela Odongo <celesteodongo@gmail.com>
2026-09-23 18:45:56 +0300

    added a comment

--- meal.py
+++ meal.py
@@ -1,6 +1,7 @@
 """ 
     In meal.py, implement a program that prompts the user for a time and outputs whether it's breakfast time, lunch time, or dinner time. 
     If it's not time for a meal, don't output anything at all. 
+    Breakfast: 07:00-08:00, Lunch: 12:00-13:00, dinner: 18:00-19:00
     Assume that the user's input will be formatted in 24-hour time as #:## or ##:##. 
     And assume that each meal's time range is inclusive. 
     For instance, whether it's 7:00, 7:01, 7:59, or 8:00, or anytime in between, it's time for breakfast.
